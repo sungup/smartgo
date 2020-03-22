e1f321d41c383df7350f26b71203298c35f7caa9
Sungup Moon <sungup@me.com>
2020-03-22 13:34:57 +0900

    wip: device file open check.

---
+++ internal/common_test.go
@@ -0,0 +1,16 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"unsafe"
+)
+
+func TestSizeOfDevIdentify(t *testing.T) {
+	a := assert.New(t)
+
+	size := unsafe.Sizeof(DevIdentify{})
+
+	a.Equal(512, int(size))
+
+}
--- internal/ioctl_test.go
+++ internal/ioctl_test.go
@@ -2,18 +2,23 @@ package internal
 
 import (
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/sys/unix"
 	"os"
 	"testing"
 )
 
 func TestIoCtl(t *testing.T) {
+	/*
+	 */
+	// Not working on MacOSX
 	a := assert.New(t)
 
 	// 1. open device file
 	testDevice := "/dev/disk0" // default MacBook device file
 	//_, err := os.Open(testDevice)
-	_, err := unix.Open(testDevice, os.O_RDWR, 0640)
-	a.NoError(err)
+	//_, err := unix.Open(testDevice, os.O_RDWR, 0640)
+	//a.NoError(err)
 
+	stat, err := os.Stat(testDevice)
+	a.NoError(err)
+	t.Log(stat.Mode())
 }
